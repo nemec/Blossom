a8e1d6db9668e67a5d25b66c9542ad1305cbdcb6
Dan Nemec <djnemec@gmail.com>
2012-09-12 01:47:06 -0500

    Added new example script file.

---
+++ Blossom.Scripting/ExampleScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Blossom.Deployment;
+using System.Collections.Generic;
+
+public class DeploymentConfig : IDeploymentConfig
+{
+    private List<Host> _hosts = new List<Host>
+    {
+        new Host
+        {
+            Hostname = "192.168.1.1",
+            Username = "username",
+            Password = "password"
+        }
+    };
+
+    public List<Host> Hosts
+    {
+        get { return _hosts; }
+        set { _hosts = value; }
+    }
+}
+
+[Deployment]
+public class Test
+{
+    [Task]
+    public void Run(IDeploymentContext context)
+    {
+        context.Logger.Info(context.Operations.RunCommand("ls"));
+    }
+}
